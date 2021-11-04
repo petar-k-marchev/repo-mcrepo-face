b0f64aa5afc5fa8b1626c9abcf78cf3d523356a9
anjPetrov <antoan.petrov@progress.com>
2021-11-04 17:44:59 +0200

    there is already a conflict

--- SafeMathApp/SafeMathApp/Program.cs
+++ SafeMathApp/SafeMathApp/Program.cs
@@ -15,7 +15,7 @@ namespace SafeMathApp
         private static void PrintTest1()
         {
             Console.WriteLine("1 + 2 = {0}", 1 + 2);
-            Console.WriteLine("safe 1 + 2 = {0}", SafeMath.SafeSum(1, 2));
+            Console.WriteLine("safe 187 + 222 = {0}", SafeMath.SafeSum(187, 222));
             Console.WriteLine("1 + double.NaN = {0}", 1 + double.NaN);
             Console.WriteLine("safe 1 + double.NaN = {0}", SafeMath.SafeSum(1, double.NaN));
             Console.WriteLine("safe double.NaN + double.NaN = {0}", SafeMath.SafeSum(double.NaN, double.NaN));
