3d639fc3ec2477e217de96b1ced4b2e4c01784a0
petar.k.marchev <petar.marchev@progress.com>
2021-11-04 14:16:53 +0200

    Add SafeMath app.

---
+++ SafeMathApp/SafeMathApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SafeMathApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("1 + 2 = {0}", SafeMath.SafeAdd(1, 2));
+            Console.WriteLine("1 + double.NaN = {0}", SafeMath.SafeAdd(1, double.NaN));
+
+        }
+    }
+}
