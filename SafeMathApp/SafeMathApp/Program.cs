71db38051094f45609ed7033c7223d14ffadbb90
petar.k.marchev <petar.marchev@progress.com>
2021-11-04 14:43:25 +0200

    Add test code.

--- SafeMathApp/SafeMathApp/Program.cs
+++ SafeMathApp/SafeMathApp/Program.cs
@@ -8,9 +8,15 @@ namespace SafeMathApp
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("1 + 2 = {0}", SafeMath.SafeAdd(1, 2));
-            Console.WriteLine("1 + double.NaN = {0}", SafeMath.SafeAdd(1, double.NaN));
+            PrintTest1();
+        }
 
+        private static void PrintTest1()
+        {
+            Console.WriteLine("1 + 2 = {0}", 1 + 2);
+            Console.WriteLine("safe 1 + 2 = {0}", SafeMath.SafeAdd(1, 2));
+            Console.WriteLine("1 + double.NaN = {0}", 1 + double.NaN);
+            Console.WriteLine("safe 1 + double.NaN = {0}", SafeMath.SafeAdd(1, double.NaN));
         }
     }
 }
